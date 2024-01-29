4bc81829ede955cb96fdadf0e09cebb9583f7e0b
Alex <alex.algarate@gmail.com>
2024-01-29 09:19:25 +0100

    prueba estados con despligue en Railway

--- remote_build.sh
+++ remote_build.sh
@@ -2,7 +2,7 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 rm -fr public
 reflex init
-reflex export --frontend-only
-# API_URL=https://wedding-backend.up.railway.app reflex export --frontend-only
+# reflex export --frontend-only
+API_URL=https://wedding-backend.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
